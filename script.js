9e3b69f43506ff062d8218385555d657f10ff97a
Dream2aker <Nataikth85@gmail.com>
2019-10-24 15:39:58 +0300

    D2_3019.10.24
    Added some functions in script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+function LinksSetColor(color){
+  var acolor = document.querySelectorAll('a');
+  i = 0;
+  while(i < acolor.length){
+  acolor[i].style.color = color;
+  i = i + 1;
+  }
+}
+function BodySetBackgroundColor(color){
+  document.querySelector('body').style.backgroundColor = color;
+}
+function BodySetColor(color){
+  document.querySelector('body').style.color = color;
+}
 function NightDayHandler(self){
    var target = document.querySelector('body');
      if(self.value === 'night'){
-       target.style.backgroundColor = 'black';
-       target.style.color = 'white';
+       BodySetBackgroundColor('black');
+       BodySetColor('white');
        self.value = 'day';
-       var acolor = document.querySelectorAll('a');
-       i = 0;
-       while(i < acolor.length){
-       acolor[i].style.color = 'powderblue';
-       i = i + 1;
+       LinksSetColor('powderblue')
        }
-     }
      else {
-       target.style.backgroundColor = 'white';
-       target.style.color = 'black';
+       BodySetBackgroundColor('white');
+       BodySetColor('black');
        self.value = 'night';
-       var acolor = document.querySelectorAll('a');
-       i = 0;
-       while(i < acolor.length){
-       acolor[i].style.color = 'black';
-       i = i + 1;
-       }
-     }
-   }
+       LinksSetColor('black');
+      }
+ }
