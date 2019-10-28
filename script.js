e03af9f899016bf39daced5f39a2ed967d2e87bd
Dream2aker <Nataikth85@gmail.com>
2019-10-28 11:32:19 +0200

    D2_3019.10.28
    Modified some code by using jQuery library.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var Links = {
     setColor:function(color){
-      var acolor = document.querySelectorAll('a');
-      i = 0;
-      while(i < acolor.length){
-      acolor[i].style.color = color;
-      i = i + 1;
-      }
+      //var acolor = document.querySelectorAll('a');
+      //i = 0;
+      //while(i < acolor.length){
+      //acolor[i].style.color = color;
+      //i = i + 1;
+      //}
+        $('a').css('color', color); // jQuery for link text color
     }
 }
 var Body = {
   setBackgroundColor:function (color){
-    document.querySelector('body').style.backgroundColor = color;
+    //document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color); //jQuery for body background color
   },
   setColor:function (color){
-  document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color); //jQuery for body text color
   }
 }
 function NightDayHandler(self){
@@ -22,7 +25,7 @@ function NightDayHandler(self){
        Body.setBackgroundColor('black');
        Body.setColor('white');
        self.value = 'day';
-       Links.setColor('powderblue')
+       Links.setColor('white')
        }
      else {
        Body.setBackgroundColor('white');
